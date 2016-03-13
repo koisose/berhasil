8b3965598877d4f31a8bba7c6bdc610e6f5bc25e
polo <slafe29@yandex.com>
2016-03-13 11:09:23 +0000

    make it better

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 echo "heroku"
 ?>
 <title>Percobaan </title>
-<link rel="shortcut icon" type="image/png" href="./img/WhatsApp.ico"/>
+<link rel="shortcut icon" type="image/png" href="../img/WhatsApp.ico"/>
